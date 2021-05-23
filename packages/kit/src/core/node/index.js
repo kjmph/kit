18c156f01b48b807755bf76cb885c776c16a4d5a
Luke Edwards <luke.edwards05@gmail.com>
2021-05-23 09:06:28 -0700

    fix: ensure `content-length` value is not exceeded

--- packages/kit/src/core/node/index.js
+++ packages/kit/src/core/node/index.js
@@ -14,29 +14,33 @@ export function getRawBody(req) {
 
 		const length = Number(h['content-length']);
 
-		/** @type {Uint8Array} */
-		let data;
+		// https://github.com/lukeed/polka/blob/next/packages/parse/index.js#L17
+		// https://github.com/jshttp/type-is/blob/c1f4388c71c8a01f79934e68f630ca4a15fffcd6/index.js#L81-L95
+		if (isNaN(length) && h['transfer-encoding'] == null) {
+			return fulfil(null);
+		}
 
-		if (!isNaN(length)) {
-			data = new Uint8Array(length);
+		let data = new Uint8Array(length || 0);
 
-			let i = 0;
-
+		if (length > 0) {
+			let offset = 0;
 			req.on('data', (chunk) => {
-				data.set(chunk, i);
-				i += chunk.length;
+				const new_len = offset + Buffer.byteLength(chunk);
+
+				if (new_len > length) {
+					return reject({
+						status: 413,
+						reason: 'Exceeded "Content-Length" limit'
+					});
+				}
+
+				data.set(chunk, offset);
+				offset = new_len;
 			});
 		} else {
-			// https://github.com/jshttp/type-is/blob/c1f4388c71c8a01f79934e68f630ca4a15fffcd6/index.js#L81-L95
-			if (h['transfer-encoding'] === undefined) {
-				return fulfil(null);
-			}
-
-			data = new Uint8Array(0);
-
 			req.on('data', (chunk) => {
 				const new_data = new Uint8Array(data.length + chunk.length);
-				new_data.set(data);
+				new_data.set(data, 0);
 				new_data.set(chunk, data.length);
 				data = new_data;
 			});
